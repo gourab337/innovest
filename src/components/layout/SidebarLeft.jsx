cc6597c030cb212a95dc892814e1a4c955ab417c
Divyansh Tripathi <divyansh.tripathi@ayushpay.com>
2023-12-08 23:56:23 +0530

    modified left bar

--- src/components/layout/SidebarLeft.jsx
+++ src/components/layout/SidebarLeft.jsx
@@ -1,14 +1,15 @@
 function SidebarLeft() {
   return (
-    <aside className=' z-40 w-full h-screen  bg-black border-r border-gray-800 '>
+    <aside className=' z-40 w-full h-screen  bg-black border-r border-gray-800 flex justify-center'>
       <div className='px-4 py-5 flex flex-col mt-4 gap-2'>
         <img
-          className='inline-block h-10 w-10 rounded-full ring-2 ring-white '
+          className='inline-block rounded-full h-16 w-16 ring-2 ring-white '
           src='https://profile.coinbase.com/images/default_avatars/Frame-12.svg'
           alt=''
+          // style={{height:'64px',width:'64px'}}
         />
         <div>
-          <h2 className='text-white font-bold'>GC Lowdi</h2>
+          <h1 className='text-white font-bold '>GC Lowdi</h1>
         </div>
         <div></div>
       </div>
