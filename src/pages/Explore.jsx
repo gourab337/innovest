644d4fb08334d5b870f4af49c35eda99a7fddae6
Divyansh Tripathi <divyansh.tripathi@ayushpay.com>
2023-12-09 06:50:13 +0530

    implemented details page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,29 +1,29 @@
 import Nav from '../components/nav/Nav';
 
+
 function Explore() {
   const data = [1, 2, 3, 4, 5, 6];
   const cardsPerRow = 4;
-
   const renderCards = () => {
     const rows = [];
     for (let i = 0; i < data.length; i += cardsPerRow) {
       const rowCards = data.slice(i, i + cardsPerRow).map((ele, index) => (
         <div
           key={i + index}
-          className="flex-1 max-w-xs bg-black border border-gray-200 rounded-lg shadow dark:bg-black dark:border-gray-700 m-2"
+          className="flex-1 max-w-xs  border border-gray-200 rounded-lg shadow  dark:border-gray-700 m-2  bg-opacity-0"
         >
-          <div className="max-w-sm bg-black border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="max-w-sm  border border-gray-200 rounded-lg shadow  dark:border-gray-700  bg-opacity-0 ">
             <a href="#">
               <img className="rounded-t-lg w-full" src='https://profile.coinbase.com/images/default_avatars/Frame-12.svg' alt="" />
             </a>
             <div className="p-5">
               <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white  bg-opacity-0">Noteworthy technology acquisitions 2021</h5>
               </a>
               <div className='mb-24'>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400  bg-opacity-0">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
               </div>
-              <a href="#" className='text-blue-700'>
+              <a href="#" className='text-blue-700' >
                 Read more
                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
--- src/router.jsx
+++ src/router.jsx
@@ -3,11 +3,16 @@ import { createBrowserRouter } from 'react-router-dom';
 import Profile from './pages/profile';
 import Layout from './components/layout/Layout';
 import Explore from './pages/Explore';
+import Details from './pages/Details';
 
 const router = createBrowserRouter([
   {
     path: '/explore',
     element: <Explore />,
+  },
+  {
+    path: '/explore/details',
+    element: <Details />,
   },
   {
     path: '/',
